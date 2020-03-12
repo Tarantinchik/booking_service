639ef22dc46bd0f2c4f102ea0a5ebd14dae36c77
Dmitry Pinchuk <pinchuk.dap@gmail.com>
2020-03-12 09:44:54 +0200

    dima todo

--- src/main/java/com/bookingservice/Main.java
+++ src/main/java/com/bookingservice/Main.java
@@ -18,6 +18,9 @@ public class Main {
         BookingControllerImpl bookingController = new BookingControllerImpl();
         FlightControllerImpl flightController = new FlightControllerImpl();
 
+        //TODO
+        //Реализовать автосоздание тестовых рейсов с датами от СЕГОДНЯ, сохраняя при этом архив старых
+
         FileReaderImpl fileReader = new FileReaderImpl();
         boolean isUsersAdded = fileReader.addUsers("src/main/data/users.txt", userController);
         boolean isFlightsAdded = fileReader.addFlights("src/main/data/flights.txt", flightController);
